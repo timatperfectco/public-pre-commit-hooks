e72d079ff0347cdc9ab28149adc8a80860b5be27
Tim Heilman <tdh@perfectco.com>
2020-11-23 12:45:35 -0800

    fix: better invocation

--- puc-cloud-pretty-checker.rb
+++ puc-cloud-pretty-checker.rb
@@ -5,14 +5,17 @@ require 'open3'
 js_ts_files = ARGV.filter do |filename|
   filename =~ /(\.ts|\.js)$/
 end
-command = 'node node_modules/eslint/bin/eslint.js -c ./.eslintrc.js --ext .ts '\
-          '--ext .js --fix --max-warnings=0 '
-command += "\"#{js_ts_files.join(' ')}\"" unless js_ts_files.empty?
-stdout, stderr, status = Open3.capture3(command)
-exit_code = status.exitstatus
-puts stdout
-if stderr != ''
-  puts 'STDERR:'
-  puts stderr
+if js_ts_files.empty?
+  exit 0
+else
+  command = 'node node_modules/eslint/bin/eslint.js -c ./.eslintrc.js --fix '\
+            "--max-warnings=0 #{js_ts_files.join(' ')}"
+  stdout, stderr, status = Open3.capture3(command)
+  exit_code = status.exitstatus
+  puts stdout
+  if stderr != ''
+    puts 'STDERR:'
+    puts stderr
+  end
+  exit exit_code
 end
-exit exit_code
